4a98072d6acb4c8fd21c105ce381facfad47d51f
Sushil Mishra <sushilmishra9699@gmail.com>
2025-06-29 12:41:26 +0530

    restructuring the backend files

---
+++ expressway-backend/src/main/java/com/sushil/expressway/ExpresswayApplication.java
@@ -0,0 +1,13 @@
+package com.sushil.expressway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpresswayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpresswayApplication.class, args);
+	}
+
+}
---
+++ expressway-backend/src/test/java/com/sushil/expressway/ExpresswayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sushil.expressway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpresswayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
