05f5d51734c17a05436b38962008829a7a870391
Sushil Mishra <sushilmishra9699@gmail.com>
2025-07-08 11:51:51 +0530

    Enhance AuthenticationRequest model with validation annotations for email and password

--- expressway-backend/src/main/java/com/sushil/expressway/models/AuthenticationRequest.java
+++ expressway-backend/src/main/java/com/sushil/expressway/models/AuthenticationRequest.java
@@ -1,5 +1,9 @@
 package com.sushil.expressway.models;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
@@ -9,7 +13,12 @@ import lombok.Setter;
 @RequiredArgsConstructor
 public class AuthenticationRequest {
 
+    @Email(message = "Please enter a valid email")
+    @NotEmpty(message = "email is mandatory")
+    @NotBlank(message = "email is mandatory")
     private String email;
+
+    @Size(min = 8, message = "Password should be 8 characters long minimum")
     private String password;
 
 }
